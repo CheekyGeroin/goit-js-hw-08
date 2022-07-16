9c3fe68b01c054930efb38b8785ef060a6837610
Danylo Altukhov <dan.97198@gmail.com>
2022-07-16 20:50:08 +0300

    done third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+
+refs.form.addEventListener('submit', onFormData);
+refs.input.addEventListener('input', throttle(onTextarea, 500));
+refs.textarea.addEventListener('input', throttle(onTextarea, 500));
+
+fillGaps();
+function onFormData(e) {
+  e.preventDefault();
+
+  e.currentTarget.reset();
+
+  console.log(localStorage.getItem(STORAGE_KEY));
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextarea() {
+  const email = refs.input.value;
+  const textarea = refs.textarea.value;
+  const formData = {
+    email,
+    textarea,
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function fillGaps() {
+  const savedForm = localStorage.getItem(STORAGE_KEY);
+  const parsedForm = JSON.parse(savedForm);
+  if (savedForm) {
+    refs.input.value = parsedForm.email;
+    refs.textarea.value = parsedForm.textarea;
+  }
+}
