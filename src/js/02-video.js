0534bd7ac9ce8d23400432312253e400f54cf95f
Danylo Altukhov <dan.97198@gmail.com>
2022-07-18 14:02:28 +0300

    done second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+player.on(
+  'timeupdate',
+  throttle(function () {
+    player
+      .getCurrentTime()
+      .then(function (seconds) {
+        localStorage.setItem('videoplayer-current-time', seconds);
+      })
+      .catch(function (error) {});
+  }, 1000),
+  player
+    .setCurrentTime(savedTime)
+    .then(function (seconds) {})
+    .catch(function (error) {})
+);
